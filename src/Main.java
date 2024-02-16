46e59219eb84409ae4e4646eba964edfdef24c5e
WallaceHonorio <wallace.ho@hotmail.com>
2024-02-15 22:04:29 -0300

    Learning: - Encapsulation: controlling access to the attributes and methods of a class; - What are public and private access modifiers; - Attribute reading methods, which are getter methods; - Attribute modification methods, which are setter methods; - Using the import keyword to import classes from other packages; - The use of the keyword this;

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
+import honorio.screenmatch.models.Film;
+
 public class Main {
     public static void main(String[] args) {
         Film film = new Film();
 
-        film.name = "The Matrix";
-        film.releaseYear = 1999;
-        film.durationMinutes = 135;
-        film.includedPlan = true;
+        film.setName("The Matrix");
+        film.setReleaseYear(1999);
+        film.setDurationMinutes(135);
+        film.setIncludedPlan(true);
 
         film.showDescription();
         film.evaluate(9);
