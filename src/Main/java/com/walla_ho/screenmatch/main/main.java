c6a208455f0cd7b6eb4434b4e53fb527c3959d18
Wallace Honorio <wallace.ho@hotmail.com>
2024-09-25 21:49:42 -0300

    Include static

--- src/Main/java/com/walla_ho/screenmatch/main/main.java
+++ src/Main/java/com/walla_ho/screenmatch/main/main.java
@@ -9,10 +9,8 @@ import com.walla_ho.screenmatch.service.DataConvert;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class main {
@@ -42,6 +40,7 @@ public class main {
 //        seasons.forEach(System.out::println);
 
 
+        //=================================================================================
 //        for( int s = 0; s < serie.totalSeasons(); s++ ){
 //            List<DataEpisode> episodes = seasons.get(s).episodes();
 //            for( int e = 0; e < seasons.size(); e++ ){
@@ -51,17 +50,23 @@ public class main {
         //Objeto(parametro -> expressaop)
 //        seasons.forEach(s -> s.episodes().forEach(e -> System.out.println(e.title())));
 
-//        //toList = Lista Imutavel
-//        //Collectors.toList() = Lista Mutavel
+        //=================================================================================
+        //toList = Lista Imutavel
+        //Collectors.toList() = Lista Mutavel
 //        List<DataEpisode> dataEpisodes = seasons.stream()
 //                .flatMap(s -> s.episodes().stream())
 //                .toList(); //.collect(Collectors.toList());
-
+//
 //        System.out.println("\nTop 5 episodes:");
-//        episodes.stream()
+//        dataEpisodes.stream()
 //                .filter(e -> !e.imdbRating().equalsIgnoreCase("N/A"))
+//                .peek(e -> System.out.println("First filter (N/A) " + e))
 //                .sorted(Comparator.comparing(DataEpisode::imdbRating).reversed())
+//                .peek(e -> System.out.println("Ordenation " + e))
 //                .limit(5)
+//                .peek(e -> System.out.println("Limit " + e))
+//                .map(e -> e.title().toUpperCase())
+//                .peek(e -> System.out.println("Map " + e))
 //                .forEach(System.out::println);
 
         System.out.println("\nEpisodes list:");
@@ -72,20 +77,52 @@ public class main {
 
         episodes.forEach(System.out::println);
 
-        System.out.println("\nFrom what year do you want to see the episodes?");
-        var year = read.nextInt();
-        read.nextLine();
+        //=================================================================================
+//        System.out.println("\nFrom what year do you want to see the episodes?");
+//        var year = read.nextInt();
+//        read.nextLine();
+//
+//        LocalDate searchDate = LocalDate.of(year, 1, 1);
+//
+////         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+//
+//        episodes.stream()
+//                .filter(e -> e.getReleased() != null && e.getReleased().isAfter(searchDate))
+//                .forEach(e -> System.out.println(
+//                        "Temporada: " + e.getSeason() +
+//                                ", Episode: " + e.getTitle() +
+//                                ", Released: " + e.getReleased()
+//                ));
 
-        LocalDate searchDate = LocalDate.of(year, 1, 1);
+        //=================================================================================
+//        System.out.println("\nWhat episode name you want:");
+//        var partTitle = read.nextLine();
+//
+//        Optional<Episode> first = episodes.stream()
+//                .filter(e -> e.getTitle().toUpperCase().contains(partTitle.toUpperCase()))
+//                .findFirst();
+//
+//        if(first.isPresent()){
+//            System.out.println("Founded episode!");
+//            System.out.println("Season: " + first.get().getSeason());
+//        } else {
+//            System.out.println("Founded not episode!");
+//        }
 
-//         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        Map<Integer, Double> ratingBySeason = episodes.stream()
+                .filter(e -> e.getImdbRating() > 0.0)
+                .collect(Collectors.groupingBy(Episode::getSeason,
+                        Collectors.averagingDouble(Episode::getImdbRating)));
 
-        episodes.stream()
-                .filter(e -> e.getReleased() != null && e.getReleased().isAfter(searchDate))
-                .forEach(e -> System.out.println(
-                        "Temporada: " + e.getSeason() +
-                                ", Episode: " + e.getTitle() +
-                                ", Released: " + e.getReleased()
-                        ));
+        System.out.println(ratingBySeason);
+
+        DoubleSummaryStatistics est = (DoubleSummaryStatistics) episodes.stream()
+                .filter(e -> e.getImdbRating() > 0.0)
+                .collect(Collectors.summarizingDouble(Episode::getImdbRating));
+
+        System.out.println("Average: " + est.getAverage());
+        System.out.println("Best: " + est.getMax());
+        System.out.println("Worst: " + est.getMin());
+        System.out.println("Amount: " + est.getCount());
     }
 }
